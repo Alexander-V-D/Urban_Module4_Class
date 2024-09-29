b82310d6afc772aeaff09b3da69811fbe2717473
Alexander-V-D <86641823+Alexander-V-D@users.noreply.github.com>
2024-09-29 11:38:21 +0700

    master

--- src/Pet.kt
+++ src/Pet.kt
@@ -1,9 +1,9 @@
 class Pet(val name: String, val weight: Int, private val type: Type) {
     fun getType() = this.type.type
-    enum class Type (val type: String) {
-        Cow ("Корова"),
-        Goose ("Гусь"),
-        Dog ("Собака"),
-        Cat ("Кошка");
+    enum class Type(val type: String) {
+        Cow("Корова"),
+        Goose("Гусь"),
+        Dog("Собака"),
+        Cat("Кошка");
     }
 }
